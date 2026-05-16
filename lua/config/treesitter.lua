c3a14099928ef9e2d9c7dfa332ccb289a008db5a
Martin Barreau <martin.barreau@gmail.com>
2026-05-16 17:10:26 +0200

    Bump packages & treesitter package name fix

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,4 +1,7 @@
-require("nvim-treesitter.config").setup({
+-- FIXME: Might be worth checking why there is no "s" when using this package on darwin system
+local pkg_name = vim.g.is_mac and "nvim-treesitter.configs" or "nvim-treesitter.config"
+
+require(pkg_name).setup({
     ensure_installed = { "python", "cpp", "lua", "vim", "rust", "go", "json", "jsonc", "heex", "eex", "css", "elixir", "html" },
     ignore_install = {}, -- List of parsers to ignore installing
     highlight = {
