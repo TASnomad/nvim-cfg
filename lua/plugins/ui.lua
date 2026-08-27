c7585be86464aa75ad2117b982e4304bb5fe258a
Martin Barreau <martin.barreau@gmail.com>
2026-08-27 16:39:54 +0200

    Better noice options

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -115,7 +115,7 @@ return {
         opts = {
             cmdline = {
                 enabled = true,
-                view = "cmdline_popup",
+                view = "cmdline",
 
                 format = {
                     cmdline = {
@@ -139,15 +139,16 @@ return {
                     },
                 },
             },
-
             popupmenu = {
-                backend = "nui"
+                enabled = true,
+                backend = "cmp"
             },
-
             presets = {
-                command_palette = true,
-                long_message_to_split = true,
-                lsp_doc_border = true,
+                bottom_search = true,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false,       -- add a border to hover docs and signature help
             },
         },
     },
